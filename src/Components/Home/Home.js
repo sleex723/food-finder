5f5abb721ead3b02584c982f5b4926f4c9080f1e
sleex89 <samlee723@gmail.com>
2018-05-05 20:36:15 -0700

    Commit before eject

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styles from './Home.css';
+
+class Home extends Component {
+  state = {}
+  render() {
+    return(
+      <div className={styles.Container}>
+      </div>
+    )
+  }
+}
+
+export default Home
