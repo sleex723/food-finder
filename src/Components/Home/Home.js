4ac06c1ac4492ea81500d359706a229e9a2fa7db
sleex89 <samlee723@gmail.com>
2018-05-05 21:31:10 -0700

    Got general layout of Home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
+import { form, FormGroup, ControlLabel, FormControl, HelpBlock } from 'react-bootstrap';
 import styles from './Home.css';
+import Logo from '../../img/foodfinder.png'
 
 class Home extends Component {
-  state = {}
+  state = {
+    search: '',
+    zipcode: ''
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
   render() {
     return(
       <div className={styles.Container}>
+        <div className={styles.Main}>
+          <img className={styles.Logo} src={Logo}/>
+          <div className={styles.Search}>
+            <div className={styles.Find}>Find</div>
+            <form>
+              <FormGroup
+                controlId="formBasixText"
+                bsSize="lg"
+              >
+              <FormControl
+                className={styles.FormCategory}
+                type="text"
+                name="search"
+                value={this.state.search}
+                placeholder="Add categories"
+                onChange={this.handleChange}
+              />
+              </FormGroup>
+            </form>
+            <div className={styles.Zipcode}>Zipcode</div>
+            <FormGroup
+                controlId="formBasixText"
+                bsSize="lg"
+              >
+              <FormControl
+                className={styles.FormCategory}
+                type="text"
+                name="zipcode"
+                value={this.state.zipcode}
+                placeholder="Enter your zipcode"
+                onChange={this.handleChange}
+              />
+              </FormGroup>
+              <button className={styles.SearchButton}><span class="glyphicon glyphicon-search"/></button>
+          </div>
+        </div>
       </div>
     )
   }
